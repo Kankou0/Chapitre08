f33c2e35ac34b0793de96392a04c187eef956d53
I1-P223 <yaojeanmarc906@gmail.com>
2026-05-28 10:37:45 +0000

    Premier commit Methode shelfEmptyWhenNoBookAdded ajoutée Deuxième test La methode add a été améliorée La méthode emptyBookShelfWhenAddIsCalledWithoutBooks ajoutée return modifié

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -11,7 +11,7 @@ public class BookShelf {
     private final List<String> books = new ArrayList<>();
 
     public List<String> books() {
-        return books;
+        return Collections.unmodifiableList(books);
     }
 
     public void add(String... booksToAdd) {
