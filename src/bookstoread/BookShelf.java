dbe4ba5b03d45d380a0bbe167e696218de6a0f4e
Kankou0 <andyeffossou14@gmail.com>
2026-06-08 11:24:21 +0000

    validation du projet

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,23 +1,37 @@
-
-
 package bookstoread;
 
+import java.time.Year;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class BookShelf {
-    private final List<String> books = new ArrayList<>();
+    private final List<Book> books = new ArrayList<>();
 
-    public List<String> books() {
-        return Collections.unmodifiableList(books);
+    public List<Book> books() {
+        return  Collections.unmodifiableList(books);
     }
 
-    public void add(String... booksToAdd) {
+    public void add(Book... booksToAdd) {
         books.addAll(Arrays.asList(booksToAdd));
     }
 
-    public List<String> arrange()
-    {
-            return books.stream().sorted().collect(Collectors.toList());
+    public List<Book> arrange() {
+        return  arrange(Comparator.naturalOrder());
     }
+    public List<Book> arrange(Comparator<Book> criteria) {
+        return books.stream().sorted(criteria).collect(Collectors.toList());
+    }
+
+
+    public Map<Year, List<Book>> groupByPublicationYear() {
+        return  books.stream().collect(Collectors.groupingBy(book ->
+                Year.of(book.getPublishedOn().getYear())));
+    }
+
+
+    public <K> Map<K, List<Book>> groupBy(Function<Book, K> fx) {
+        return books.stream().collect(Collectors.groupingBy(fx));
+    }
+
 }
